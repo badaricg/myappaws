1aa9bf6694dd5bf2600dc77cc5aa930cfe50f249
S <badari-narayana.s@capgemini.com>
2023-01-05 09:58:04 +0530

    comment changed

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello Pravin!!!";
+        return "Hello test Containerization Jan 5!!!";
     }
 
     public static void main(String[] args) throws Exception {
